9d8e51e10beb2adf5a5016f21328085b0d5c6c64
Andylisco <57235735+Andylisco@users.noreply.github.com>
2022-08-11 14:26:01 -0300

    Se incluyo get/set de empleado. Y autonumeracion constructor

--- src/Dominio/Empleado.java
+++ src/Dominio/Empleado.java
@@ -6,14 +6,39 @@ public class Empleado {
 	private String nombre;
 	private int edad;
 	static int idIni = 999;
+	
 // creacion de constructores	
 	public Empleado() {
+		idIni++;
+		this.id = idIni; 
 		this.edad = 99;
 		this.nombre = "sin nombre";
 	}
 	public Empleado(String nombre, int edad) {
+		idIni++;
 		this.id = idIni;
 		this.nombre = nombre;
 		this.edad = edad;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
 }
